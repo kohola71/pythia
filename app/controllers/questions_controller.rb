a9617e3911ca8bde6e64b1b799223b20ec525a8c
ola kohut <ola.kohut@gmail.com>
2013-07-26 10:50:17 +0200

    first commit of new project

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+
+	def index
+        puts "new"
+	end 
+
+	def new
+		render :new 
+	end
+
+	def create 
+	end 
+end 
+
+
+
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PythiaApp::Application.config.session_store :cookie_store, key: '_pythia_app_session'
