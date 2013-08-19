d8ff4ebd7fdfe93ac6ba72edb7508dad5031bb7d
Ola Kohut <ola.kohut@gmail.com>
2013-08-19 21:03:49 +0200

    can ask questions and get yes or no answers without users

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,20 +1,34 @@
 class AnswersController < ApplicationController
 
-
 	def create 
-		@question = Question.find(params[:question_id])
-		
-		redirect_to question_path(@question)
+	# @question = Question.find(params[:answer][:question_id])	
+		@answer = Answer.new(answer_params)
+		@answer.save
+		redirect_to answer_path(@answer)
 	end 
 
 
-		def destroy
-			@question = Question.find(params[:question_id])
-			@answer = @question.answers.find(params[:id])
-			@answer.destroy
-			redirect_to question_path(@question)
-		end
+	def show
+		@answer = Answer.find(params[:id])
+		@question = Question.find(@answer.question_id)
+
+		@yes_answers = Answer.where(question_id: @answer.question_id, body: 'YES').count
+		@no_answers = Answer.where(question_id: @answer.question_id, body: 'NO').count
+
+	end 
 
 
+	def destroy
+		@question = Question.find(params[:question_id])
+		@answer = @question.answers.find(params[:id])
+		@answer.destroy
+		redirect_to question_path(@question)
+	end
+
+private 
+
+def answer_params
+      params.require(:answer).permit(:body, :question_id)
+    end
 
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,6 +24,7 @@ class QuestionsController < ApplicationController
 
 	def show 
 		@question = Question.find(params[:id])
+		@answer = Answer.new
 	end 
 
 
